1639ede268bcc4467045c8e965f89884e6d199a8
Mike <mikewrubel51@gmail.com>
2021-04-12 16:22:05 -0400

    commit tree 9c4259e93104a2a591156c4f0ea37c66a4add2f9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        
+        messages.push('Thank you, ' + names[i] + ', for the wonderful ' + eventName + ' gift!')
+    }
+    return messages
+}
+function countDown(){
+    for (let cDown = 10; cDown >= 0; cDown--) {
+    console.log(cDown);
+  }
+}
